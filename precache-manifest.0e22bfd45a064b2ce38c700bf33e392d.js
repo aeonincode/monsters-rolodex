f704d5d009faa77ad29fe3ab898de0dd4df0e866
khashayar <khashayar.sasanni@hotmail.com>
2019-08-17 13:11:09 +0200

    Updates

---
+++ precache-manifest.0e22bfd45a064b2ce38c700bf33e392d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbdf979b6957d68f38f89a04f4e269c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "09f9d1c19be3ed6e9fb6",
+    "url": "/monsters-rolodex/static/css/main.cfc4c036.chunk.css"
+  },
+  {
+    "revision": "607e395712115653f754",
+    "url": "/monsters-rolodex/static/js/2.45dda9c7.chunk.js"
+  },
+  {
+    "revision": "09f9d1c19be3ed6e9fb6",
+    "url": "/monsters-rolodex/static/js/main.79d4cf8b.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
